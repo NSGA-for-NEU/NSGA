6fdc591d7bfdd921d860e920e77eb24f31043d3e
fangkjfff <146603482+fangkjfff@users.noreply.github.com>
2024-01-22 19:16:08 +0800

    fname.m

--- Constrained NSGA2/fname.m
+++ Constrained NSGA2/fname.m
@@ -20,42 +20,51 @@ function [fit, err] = object_fun(x)
     %% Unconstrained Test functions (for p=1 to p=9)
     if p==1     % Test case problem 1
         f1=(4*x(1)^2)+(4*x(2)^2);                       
-        f2=((x(1)-5)^2)+((x(2)-5)^2);               
+        f2=((x(1)-5)^2)+((x(2)-5)^2);
+        
     elseif p==2     % ZDT1 from Deb paper NSGA2
         cons=[0];
         f1 = x(1);
         g=1+(9*sum(x(2:V),2)/(V-1));            
-        f2 = g*(1-sqrt(x(1)/g));                  
+        f2 = g*(1-sqrt(x(1)/g));  
+        
     elseif p==3     % kUR from Deb
         f1=(-10*exp(-0.2*(sqrt(x(1)^2+x(2)^2))))+(-10*exp(-0.2*(sqrt(x(2)^2+x(3)^2))));
         f2=((abs(x(1))^0.8) + (5*sin(x(1))^3))+((abs(x(2))^0.8) + (5*sin(x(2))^3))+((abs(x(3))^0.8) + (5*sin(x(3))^3));
+        
     elseif p==4    % SCH frm Deb paper
         f1=x.*x;
         f2=(x-2).^2;
+        
     elseif p==5     % ZDT2
         f1 = (x(1));
         g=1+(9*sum(x(2:V),2)/(V-1));             
-        f2 =((1-(x(1)/g)^2));                
+        f2 =((1-(x(1)/g)^2)); 
+        
     elseif p==6     % Test case problem 2
         f1=1-exp(-sum((x-1/sqrt(V)).^2,2));
         f2=1-exp(-sum((x+1/sqrt(V)).^2,2));
+        
     elseif p==7     % ZDT3
         f1 = x(1);                                 
         g=1+(9*sum(x(2:V),2)/(V-1));               
-        f2 = (1-(sqrt(x(1)/g)) - ((x(1)/g)*sin(10*pi*x(1))));               
+        f2 = (1-(sqrt(x(1)/g)) - ((x(1)/g)*sin(10*pi*x(1)))); 
+        
     elseif p==8     % ZDT4       
         f1 = x(1);  temp=0;
         for ii = 2: V
             temp=temp+((x(ii))^2)-(10*cos(4*pi*x(ii)));
         end
         g= 1 + (10*(V-1)) + temp;           
-        f2 = (1-sqrt(x(1)/g));                 
+        f2 = (1-sqrt(x(1)/g));
+        
     elseif p==9     % ZDT6       
         f1 = 1-(exp(-4*x(1)))*(sin(6*pi*x(1)))^6; 
         g=1+(9*(sum(x(2:V),2)/(V-1))^0.25);        
         f2 = (1-(f1/g)^2);                     
     end
     err= zeros(1,1);
+    
     %% Constrained Test functions (for p=10 to p=14)
     if p==10     %BNH 
         f1=4*(x(1)^2)+4*(x(2)^2);
@@ -63,18 +72,21 @@ function [fit, err] = object_fun(x)
         c(1,1)=(x(1)-5)^2 + x(2)^2 -25;
         c(1,2)=-(x(1)-8)^2-(x(2)+3)^2+7.7;
         err=(c>0).*c;
+        
     elseif p==11     %SRN  
         f1=(x(1)-2)^2+(x(2)-1)^2+2;
         f2=9*x(1)-(x(2)-1)^2;
         c(1,1)=x(1)^2+x(2)^2-225;
         c(1,2)=x(1)-(3*x(2))+10;
         err=(c>0).*c;
+        
     elseif p==12     %TNK
         f1=x(1);
         f2=x(2);
         c(1,1)=-x(1)^2-x(2)^2+1+(0.1*cos(16*atan((x(1)/x(2))))); 
         c(1,2)=(x(1)-0.5)^2+(x(2)-0.5)^2-0.5;
         err=(c>0).*c;
+        
     elseif p==13     % OSY 
         f1=-((25*(x(1)-2)^2)+((x(2)-2)^2)+((x(3)-1)^2)+((x(4)-4)^2)+((x(5)-1)^2));
         f2=(x(1)^2)+(x(2)^2)+(x(3)^2)+(x(4)^2)+(x(5)^2)+(x(6)^2);
@@ -85,6 +97,7 @@ function [fit, err] = object_fun(x)
         c(1,5)=-4+((x(3)-3)^2)+x(4);
         c(1,6)=-((x(5)-3)^2)-x(6)+4;
         err=(c>0).*c;
+        
     elseif p==14    % CONSTR
         f1=x(1);
         f2=(1+x(2))/(x(1));
