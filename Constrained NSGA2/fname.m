d48d8b6ffc665c38911fed4d5eb10e8a66d86a36
yanyuchen <109837367+userandnames@users.noreply.github.com>
2024-01-30 20:34:31 +0800

    完善fname， 增加自动生成随机参数excel表， 增加json参数表

--- Constrained NSGA2/fname.m
+++ Constrained NSGA2/fname.m
@@ -1,6 +1,7 @@
-function [fit, err] = fname(x)     %x:长度为x_num
+function [fit, err] = fname(x, x_max)     %x:长度为x_num
     data = xlsread('net14.xlsx');
-    time_list = data(5, : )
+    time_list = data(:, 5);
+    fee_list = data(:, 6);
     
     test = [];
     for i = 1:length(x_max)
@@ -23,7 +24,27 @@ function [fit, err] = fname(x)     %x:长度为x_num
         end
     end
     
+    filename = 'param.json';  % JSON文件名
+    jsonStr = fileread(filename);  % 读取JSON文件内容
+    jsonData = jsondecode(jsonStr);  % 解码JSON数据
+
+    yuan = jsonData.yuan;
+    time_cost1 = 0;
+    time_cost2 = 0;
     for i = 1:length(test)
-        
+        if test(i) == 1 && data(i, 1) <= yuan
+            time_cost1 = max(time_cost1, time_list(i));
+        elseif test(i) == 1 && data(i, 1) > yuan
+            time_cost2 = max(time_cost2, time_list(i));
+        end
     end
+
+    fee_cost = 0;
+    for i = 1:length(test)
+        if test(i) == 1 
+            fee_cost = fee_cost + fee_list(i);
+        end
+    end
+    %fit计算公式有待更新
+    fit = time + fee_cost;
 end
